7a85750e36ca6c7705268a1ccbc13a3ba3b19cb7
akash007inn-code <akash007.inn@gmail.com>
2026-08-28 23:29:32 +0530

    Create 58. Length of Last Words.py

---
+++ Array/58. Length of Last Words.py
@@ -0,0 +1,21 @@
+...
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+#
+  class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        i = len(s) - 1
+        
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+            
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+            
+        return length
+      
