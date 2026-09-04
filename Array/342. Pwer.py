d71293f40431a5d791d06027e40ba2beddcd7c6a
akash007inn-code <akash007.inn@gmail.com>
2026-09-04 21:42:43 +0530

    Create 342. Pwer.py

---
+++ Array/342. Pwer.py
@@ -0,0 +1,11 @@
+...
+Example 1:
+
+Input: n = 16
+Output: true
+
+#
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
+      
